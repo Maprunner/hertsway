1e93116207a42ef4ecd8565e9001c390910c5aad
Simon Errington <simon@maprunner.co.uk>
2020-12-28 21:21:35 +0000

    Summary page start

--- src/globals/stats.js
+++ src/globals/stats.js
@@ -4,13 +4,23 @@ let dist = 0
 let climb = 0
 let count = 0
 let time = 0
+let legTable = []
 for (const leg in legs) {
+  let thisLeg = {}
   count = count + 1
   dist = dist + legs[leg].distance
+  thisLeg.distance = legs[leg].distance.toFixed(1)
+  thisLeg.totDistance = dist.toFixed(1)
   climb = climb + legs[leg].climb
+  thisLeg.climb = legs[leg].climb
+  thisLeg.totClimb = climb
   time = time + legs[leg].time
+  thisLeg.time = legs[leg].time
+  thisLeg.totTime = time
+  legTable.push(thisLeg)
 }
 module.exports = {
+  legTable: legTable,
   totalLegs: count,
   totalDistance: dist.toFixed(1),
   totalClimb: climb,
